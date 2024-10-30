9b12da6daac3f4f9ef68c0bdee4b06e348fc5dae
lokx1 <longbaoluu68@gmail.com>
2024-10-30 12:14:11 +0700

    HTML-CSS

---
+++ HCMUT_SPSO_MAIN/login.js
@@ -0,0 +1,30 @@
+// login.js
+function login() {
+   
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+   
+    if (username === 'user' && password === 'password') {
+       
+        window.location.href = 'homepage.html';
+    } else {
+        
+        alert('Tên đăng nhập hoặc mật khẩu không đúng!');
+    }
+}
+// login.js
+function login() {
+   
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+   
+    if (username === 'user' && password === 'password') {
+       
+        window.location.href = 'homepage.html';
+    } else {
+      
+        alert('Tên đăng nhập hoặc mật khẩu không đúng!');
+    }
+}
